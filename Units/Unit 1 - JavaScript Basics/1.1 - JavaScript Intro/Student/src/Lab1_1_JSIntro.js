6bfb339bc3eba9fb5e11a476c001201a490cc5d6
ttaylor977 <ttaylor28397@columbushs.com>
2025-10-23 09:45:56 -0400

    Corrected Completed 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -7,59 +7,50 @@
    console.log("TyTaylor")
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
-   let favoriteFood;
+   let favoriteFood
 
    favoriteFood = "Steak"
-   console.log(favoriteFood);
+   console.log(favoriteFood)
 
    favoriteFood = "Pizza"
    console.log(favoriteFood)
 
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
-   let mystery;
+   let mystery
 
    mystery = "football"
    console.log("Value: " + mystery + " Type: " + typeof mystery)
 
-   mystery = 100;
+   mystery = 100
    console.log("Value: " + mystery + " Type: " + typeof mystery)
 
 // TODO 4: Declare const schoolName, try to reassign
-   let schoolName;
+   const schoolName = "Christopher Columbus Highschool"
 
-   schoolName = "Columbus";
-   console.log(schoolName)
-
-   schoolName = "St.Brendan"
-   console.log(schoolName)
-
+   console.log ("I attend" + schoolName)
+   
 // TODO 5: Create three valid camelCase variables and print them
-   let camelCase;
-
-   camelCase = "Llama"
-   console.log(camelCase)
-
-   camelCase = "Zebra"
-   console.log(camelCase)
-
-   camelCase = "Horse"
-   console.log(camelCase)
+   let sixSeven
+   let skyColor
+   let accountBalance
 
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
 // - Repeat using template literals
+   let age = 16
+   console.log("I am" + age + "years old") // Concatenation
 
+   console.log(`I am ${age} years old`) // Literals
 /* ===========================================================
    🎟 EXIT TICKET
    1. What new concept or skill did you learn in this lab?
          A new concept I learned in the lab is that Javascript is stricter than html and css, for ex. capital letters.
 
    2. How did you apply logic or conditionals in your code?
-         I applied logic into my code through my past lessons I've learned and following directions.
 
    3. What challenges did you encounter, and how did you solve them?
          I encountered some misstyped codes leading them to errors, I went into 'Inspect', 'Console' to locate and fix the error.
 
    4. Where else could this programming concept be useful?
-         Right now Im so early into the lesson I dont have an idea where I can use this yet.
+         For organizing.
    =========================================================== */
