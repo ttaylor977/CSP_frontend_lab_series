1c63879f031c670b39798e1a82ccc22ec38836ab
ttaylor977 <ttaylor28397@columbushs.com>
2025-10-23 09:19:57 -0400

    Completed 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -25,10 +25,25 @@
    console.log("Value: " + mystery + " Type: " + typeof mystery)
 
 // TODO 4: Declare const schoolName, try to reassign
+   let schoolName;
 
+   schoolName = "Columbus";
+   console.log(schoolName)
+
+   schoolName = "St.Brendan"
+   console.log(schoolName)
 
 // TODO 5: Create three valid camelCase variables and print them
+   let camelCase;
 
+   camelCase = "Llama"
+   console.log(camelCase)
+
+   camelCase = "Zebra"
+   console.log(camelCase)
+
+   camelCase = "Horse"
+   console.log(camelCase)
 
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
@@ -37,7 +52,14 @@
 /* ===========================================================
    🎟 EXIT TICKET
    1. What new concept or skill did you learn in this lab?
+         A new concept I learned in the lab is that Javascript is stricter than html and css, for ex. capital letters.
+
    2. How did you apply logic or conditionals in your code?
+         I applied logic into my code through my past lessons I've learned and following directions.
+
    3. What challenges did you encounter, and how did you solve them?
+         I encountered some misstyped codes leading them to errors, I went into 'Inspect', 'Console' to locate and fix the error.
+
    4. Where else could this programming concept be useful?
+         Right now Im so early into the lesson I dont have an idea where I can use this yet.
    =========================================================== */
