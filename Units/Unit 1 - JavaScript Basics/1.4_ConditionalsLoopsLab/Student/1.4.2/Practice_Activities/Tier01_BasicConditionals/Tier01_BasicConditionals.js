9bdd954087668f2005fd8612b6a939adc6d3c4c8
ttaylor977 <ttaylor28397@columbushs.com>
2025-11-12 10:20:56 -0500

    Completed  Tier 1 & 2 Lab

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
@@ -1,2 +1,24 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 1")
+
+// Grade Evaluator 
+// let grade = prompt("Enter your Grade")
+// if (grade >= 90 ) {
+//     console.log("A")
+// } else if (grade >= 80) {
+//     console.log("B")
+// } else if (grade >= 70) {
+//     console.log("C")
+// } else if (grade >= 60) {
+//     console.log("D")
+// } else {
+//     console.log("F")
+// }
+
+// Voting Eligibility 
+// let age = prompt("Enter Age")
+//     if (age >= 18) {
+//         console.log("You can vote!")
+//     } else if (age <= 18) {
+//         console.log("You cant't vote!")
+//     }
--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
@@ -1,2 +1,42 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 2")
+
+
+
+//Counting to 10
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+
+// Countodwn timer
+
+// let t = 10;
+
+// while ( t >= 0 ) {
+//     if (t == 0) {
+//         console.log("Liftoff")
+//         t--;
+//     } else {
+//         console.log("t")
+//             t--;
+//     }
+// }
+
+
+//Password Attempt
+// let secret = "Explorers";
+//     let attempt = 1
+//     let pw;
+//     do {
+//         if (attempt <= 3) {
+//             pw = prompt("What is the password?")
+//             attempt++;
+//         }
+//     } while (pw != secret && attempt <= 3);
+//     if (pw == secret) {
+//         console.log("You are logged in.")
+//     } else {
+//         console.log("Sorry youve reached your limit  ")
+//     }
