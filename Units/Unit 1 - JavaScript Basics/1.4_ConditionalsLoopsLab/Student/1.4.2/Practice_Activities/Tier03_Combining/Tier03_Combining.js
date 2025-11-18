d7149edd3c448b99b8d36317447bdda96d4cc7f9
ttaylor977 <ttaylor28397@columbushs.com>
2025-11-18 14:05:20 -0500

    Completed 1.4.2 Lab

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier03_Combining/Tier03_Combining.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier03_Combining/Tier03_Combining.js
@@ -1,2 +1,30 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
-console.log("TEST - TIER 3")
+console.log("TEST - TIER 3");
+
+//FizzBuzz
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 15 === 0) {
+//     console.log("Fizzbuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//Guess the Number
+let secret = Math.floor(Math.random() * 10) + 1;
+let guess;
+while (guess != secret) {
+    guess = Number(prompt("Guess the number 1-10"))
+    if (guess < secret) {
+        alert("Too low")
+    }
+    else if (guess > secret) {
+        alert("Too high")
+    } else {
+        alert("Congrats")
+    }
+}
--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier04_Applications/Tier04_Applications.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier04_Applications/Tier04_Applications.js
@@ -1,2 +1,45 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 4")
+
+//login system
+
+let u = "admin";
+let p = "1234";
+let username;
+let password;
+
+while (u != username && password != p) {
+    username = prompt("Enter your username:");
+    password = prompt("Enter your password:");
+
+    if ( username === u && password === p) {
+        alert("Welcome");
+        break;
+    } else {
+        alert("Invalid credentials. Try again");
+    }
+}
+
+
+
+
+
+
+
+
+
+//Weather Bot
+
+// let weather = prompt("Whats the weather")
+
+// let weatherLowerCase = weather.toLowerCase();
+
+// if (weatherLowerCase == "sunny"){
+//     alert("shirt and shorts")
+// } else if (weatherLowerCase == "cold"){
+//     alert("sweater and pants")
+// } else if (weatherLowerCase == "rainy"){
+//     alert("raincoat and boots")
+// } else {
+//     alert("not a valide response")
+// }
